1a890a16a01fd975a5ecdda359c921c187da389b
Mocasia <43439567+Mocasia@users.noreply.github.com>
2026-01-21 18:51:40 +0800

    Delete togaf-master_-30-day-plan -v2 directory

--- togaf-master_-30-day-plan -v2/env.d.ts
+++
@@ -1,7 +0,0 @@
-// This file helps TypeScript understand that process.env exists
-declare namespace NodeJS {
-  interface ProcessEnv {
-    API_KEY: string;
-    [key: string]: string | undefined;
-  }
-}
